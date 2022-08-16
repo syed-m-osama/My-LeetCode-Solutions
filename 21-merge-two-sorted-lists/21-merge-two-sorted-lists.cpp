a83047c272c19663f4c5f48b0c26d78687f07f33
syed-m-osama <83537384+syed-m-osama@users.noreply.github.com>
2022-08-17 05:16:22 +0530

    Time: 17 ms (26.03%), Space: 14.9 MB (42.65%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -10,75 +10,125 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* ptr; ListNode* res;
-        if(list1 == nullptr){return list2;}
-        if(list2 == nullptr){return list1;}
-        if(list1->val < list2->val){
-            ptr = list1;
-            list1 = list1->next;
-            ptr->next = nullptr;
+    ListNode* mergeTwoLists(ListNode* first, ListNode* second) {
+        ListNode* head; ListNode* curr;
+        if(first == nullptr){return second;}
+        if(second == nullptr){return first;}
+        
+        
+        //Node<int> *head;
+    if(first->val <= second->val){
+        head = first;
+        if(first->next == nullptr){
+            head->next = second;
+            return head;
         }
-        else if(list1->val == list2->val){            
-            ptr = list1;
-            res = ptr;
-            list1 = list1->next;
-            ptr->next = nullptr;
-            ptr->next = list2;
-            list2 = list2->next;
-            res = ptr;
-            ptr = ptr->next;
-            ptr->next = nullptr;    
+        first = first->next;
+    }
+    else{
+        head = second;
+        if(second->next == nullptr){
+            head->next = first;
+            return head;
         }
-        else{
-            ptr = list2;
-            res = ptr;
-            list2 = list2->next;
-            ptr->next = nullptr;      
-        }
-        while(true){
-            if(list1 == nullptr) {break;}
-            if(list2 == nullptr) {break;}
-            if(list1->val <= list2->val){
-                ptr->next = list1;
-                ptr = ptr->next;
-                list1 = list1->next;
-                ptr->next = nullptr;
-            }
-            // else if(list1->val == list2->val){
-            //     ptr->next = list1;
-            //     ptr = ptr->next;
-            //     list1 = list1->next;
-            //     ptr->next = list2;
-            //     ptr = ptr->next;
-            //     list2 = list2->next;
-            //     ptr->next = nullptr;                
-            // }
-            else{
-                ptr->next = list2;
-                ptr = ptr->next;
-                list2 = list2->next;
-                ptr->next = nullptr;
-            }
-        }
-        if(list1 == nullptr){
-            ptr->next = list2;
-            // while(list2 != nullptr){
-            //     ptr->next = list2;
-            //     ptr = ptr->next;
-            //     list2 = list2->next;
-            //     ptr->next = nullptr;
-            // }
+        second = second->next;
+    }
+    curr = head;
+    while(first != nullptr && second != nullptr){
+        if(first->val <= second->val){
+            head->next = first;
+            head = first;
+            first = first->next;
         }
         else{
-            ptr->next = list1;
-            // while(list1 != nullptr){
-            //     ptr->next = list1;
-            //     ptr = ptr->next;
-            //     list1 = list1->next;
-            //     ptr->next = nullptr;
-            // }
+            head->next = second;
+            head = second;
+            second = second->next;
         }
-     return res;   
+    }
+    if(first == nullptr){
+        head->next = second;
+        return curr;
+    }
+    if(second == nullptr){
+        head->next = first;
+        return curr;
+    }
+        return curr;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         if(list1->val < list2->val){
+//             ptr = list1;
+//             list1 = list1->next;
+//             ptr->next = nullptr;
+//         }
+//         else if(list1->val == list2->val){            
+//             ptr = list1;
+//             res = ptr;
+//             list1 = list1->next;
+//             ptr->next = nullptr;
+//             ptr->next = list2;
+//             list2 = list2->next;
+//             res = ptr;
+//             ptr = ptr->next;
+//             ptr->next = nullptr;    
+//         }
+//         else{
+//             ptr = list2;
+//             res = ptr;
+//             list2 = list2->next;
+//             ptr->next = nullptr;      
+//         }
+//         while(true){
+//             if(list1 == nullptr) {break;}
+//             if(list2 == nullptr) {break;}
+//             if(list1->val <= list2->val){
+//                 ptr->next = list1;
+//                 ptr = ptr->next;
+//                 list1 = list1->next;
+//                 //ptr->next = nullptr;
+//             }
+//             // else if(list1->val == list2->val){
+//             //     ptr->next = list1;
+//             //     ptr = ptr->next;
+//             //     list1 = list1->next;
+//             //     ptr->next = list2;
+//             //     ptr = ptr->next;
+//             //     list2 = list2->next;
+//             //     ptr->next = nullptr;                
+//             // }
+//             else{
+//                 ptr->next = list2;
+//                 ptr = ptr->next;
+//                 list2 = list2->next;
+//                 //ptr->next = nullptr;
+//             }
+//         }
+//         if(list1 == nullptr){
+//             ptr->next = list2;
+//             // while(list2 != nullptr){
+//             //     ptr->next = list2;
+//             //     ptr = ptr->next;
+//             //     list2 = list2->next;
+//             //     ptr->next = nullptr;
+//             // }
+//         }
+//         else{
+//             ptr->next = list1;
+//             // while(list1 != nullptr){
+//             //     ptr->next = list1;
+//             //     ptr = ptr->next;
+//             //     list1 = list1->next;
+//             //     ptr->next = nullptr;
+//             // }
+//         }
+//      return res;   
     }
 };
