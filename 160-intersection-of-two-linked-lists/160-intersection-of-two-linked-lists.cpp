cc8e3cd84e5c5424e2f71abb46ac22be75734033
syed-m-osama <83537384+syed-m-osama@users.noreply.github.com>
2022-08-18 13:19:59 +0530

    Time: 53 ms (84.75%), Space: 14.6 MB (32.88%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -8,20 +8,48 @@
  */
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *firstHead, ListNode *secondHead) {
+    ListNode *getIntersectionNode(ListNode *fir, ListNode *sec) {
         
-        while(firstHead != nullptr){
-        ListNode *temp = secondHead;
-        while(temp != nullptr){
-            if(firstHead == temp){
-                return firstHead;
-            }
-            temp = temp->next;
+    ListNode *l1 = fir;
+    ListNode *l2 = sec;
+    while(l1 != nullptr && l2 != nullptr){
+        if(l1 == l2){
+        return l1;
         }
-        firstHead = firstHead->next;
+        l1 = l1->next;
+        l2 = l2->next;
+    }
+    if(l1 == nullptr && l2 == nullptr){
+        return nullptr;
+    }
+    if(l1 == nullptr){
+        l1 = sec;
+        l2 = l2->next;
+        while(l2 != nullptr){
+            l1 = l1->next;
+            l2 = l2->next;
+        }
+        l2 = fir;
+        l1 = l1->next;
+    }
+    else if(l2 == nullptr){
+        l2 = fir;
+        l1 = l1->next;
+        while(l1 != nullptr){
+            l1 = l1->next;
+            l2 = l2->next;
+        }
+        l1 = sec;
+        l2 = l2->next; 
+    }
+    while(l1 != nullptr && l2 != nullptr){
+        if(l1 == l2){
+        return l1;
+        }
+        l1 = l1->next;
+        l2 = l2->next;
     }
     return nullptr;
-        
         
     }
 };
