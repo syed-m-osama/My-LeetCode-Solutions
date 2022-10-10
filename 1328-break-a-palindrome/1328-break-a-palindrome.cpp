91fa02dd5594a94d0ceffde9553aaab61a1eab4c
syed-m-osama <83537384+syed-m-osama@users.noreply.github.com>
2022-10-10 23:13:11 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (45.53%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     string breakPalindrome(string palindrome) {
-        if(palindrome.length() == 1) return "";
-        for(int i = 0; i< palindrome.length(); i++){
-            if(palindrome[i] != 'a' && palindrome.length()/2 != i){
+        int len = palindrome.length();
+        if(len == 1) return "";
+        for(int i = 0; i< len/2; i++){
+            if(palindrome[i] != 'a' && len/2 != i){
                 palindrome[i] = 'a';
                 return palindrome;
             }
         }
-        palindrome[palindrome.length() -1] = 'b';
+        palindrome[len -1] = 'b';
         return palindrome;
     }
 };
