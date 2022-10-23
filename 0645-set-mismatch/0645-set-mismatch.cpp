c5c2627ae0753ac469205d557b2201e8b8b28b07
syed-m-osama <83537384+syed-m-osama@users.noreply.github.com>
2022-10-23 20:01:54 +0530

    Time: 63 ms (68.81%), Space: 31 MB (21.22%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int miss = 0;
+        int repeat = 0;
+        int corr = 0;
+        unordered_set<int> set;
+        for(int i=0; i<nums.size(); i++){
+            corr = corr ^ (i+1);
+            if(set.find(nums[i]) != set.end()) repeat = nums[i];
+            else set.insert(nums[i]);
+        }
+        for(auto i:set){
+            corr = corr ^ i;
+        }
+        return vector<int> ({repeat,corr});
+    }
+};
