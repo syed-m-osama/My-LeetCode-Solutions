e2949ea593075123f29822795813d8a28a1534ec
syed-m-osama <83537384+syed-m-osama@users.noreply.github.com>
2022-10-26 23:23:53 +0530

    Time: 12 ms (78.04%), Space: 7.6 MB (75.33%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,18 +9,35 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        ListNode* ptr = head; int pos = 0;
-        unordered_set<ListNode*> ht;
-        while(ptr != nullptr){
-            cout<<ptr->val;
-            if(ht.find(ptr) != ht.end()){
-                return ptr;
-            }
-            else{
-                ht.insert(ptr);
-            }
-            ptr = ptr->next;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast != nullptr && fast->next != nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast) break;
         }
-        return nullptr;
+        if(fast == nullptr || fast->next == nullptr) return nullptr;
+        fast = head;
+        while(slow != fast){
+            slow = slow->next;
+            fast = fast->next;
+        }
+        return fast;
     }
 };
+
+
+// successful submission O(n) and O(n)
+// ListNode* ptr = head; int pos = 0;
+//         unordered_set<ListNode*> ht;
+//         while(ptr != nullptr){
+//             cout<<ptr->val;
+//             if(ht.find(ptr) != ht.end()){
+//                 return ptr;
+//             }
+//             else{
+//                 ht.insert(ptr);
+//             }
+//             ptr = ptr->next;
+//         }
+//         return nullptr;
